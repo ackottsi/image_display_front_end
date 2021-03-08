454c7012ce33c36c8aabf4be41d3fe9d66ce0e76
ackottsi <kottsickandrewc@johndeere.com>
2021-03-07 23:04:17 -0600

    started on forms for image adding

---
+++ src/component/AddImage.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import '../AddImage.css';
+
+const AddImage = (props) => {
+    console.log(props)
+
+
+    return(
+
+        <div className="AddImage">
+
+      
+            <form onSubmit={this.login}>
+                <input
+                    name='title'
+                    type='text'
+                    placeholder='title'
+                    value={this.state.username}
+                    onChange={this.loginOnChange}
+                />
+                <input
+                    name='comments'
+                    type='text'
+                    placeholder='comments'
+                    value={this.state.comments}
+                    onChange={this.loginOnChange}
+                />
+                 <input
+                    name='url'
+                    type='text'
+                    placeholder='title'
+                    value={this.state.url}
+                    onChange={this.loginOnChange}
+                />
+                <input
+                    name='date'
+                    type='date'
+                    placeholder='date'
+                    value={this.state.date}
+                    onChange={this.loginOnChange}
+                />
+                <input type='submit' value='Add Image' />
+            </form>
+
+            
+        </div>
+    );
+}
+
+export default AddImage;
