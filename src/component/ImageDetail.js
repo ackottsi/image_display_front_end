94af03aae3c46692d839fc2b6013b24a562fbf39
ackottsi <kottsickandrewc@johndeere.com>
2021-03-10 09:46:35 -0600

    refactored image details page to use hooks.  added methods to handle change and handle submit

--- src/component/ImageDetail.js
+++ src/component/ImageDetail.js
@@ -1,20 +1,66 @@
-import React from 'react';
 import '../ImageDetail.css';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 const ImageDetail=(props)=>{
-   
     const foundImage=props.imageData.find(image=>{
         console.log(image.id)
         console.log(props.match.params)
         return image.id===parseInt(props.match.params.id)
     });
+    const[state,setState]=useState({
+        title:foundImage.title,
+        comments:foundImage.comments,
+        url:foundImage.url,
+        date:foundImage.date,
+        editImage:false
+    })
+
+    const handleChange=(e)=>{
+        e.preventDefault()
+        console.log(e.target)
+        const {name,value}=e.target;
+        setState(prevState=>({
+            ...prevState,          
+            [name]:value
+        }))
+    }
+
+       const handleSubmit= async (e)=>{
+        e.preventDefault();
+        const data={
+            title:state.title,
+            comments:state.comments,
+            url:state.url,
+            date:state.date
+          }
+        setState(data)
+
+        const response= await axios.post('http://localhost:3002/images/all',data)
+        console.log(response)
+
+  }
+
+
+
+
+
+    
     
 return(
     <div className="image-detail-container">
         {foundImage ? (
             <div>
-                <h2>{foundImage.title}</h2>
-                <img src={foundImage.url}/>
+                  <form className="create-form-container" onSubmit={handleSubmit}>
+                <input
+                    name='title'
+                    type='text'
+                    placeholder='title'
+                    value={state.title}
+                    onChange={handleChange}
+                />
+                <input className="new-image-submit" type='submit' value='Add Image' />
+            </form>
             </div>
         ):
        
